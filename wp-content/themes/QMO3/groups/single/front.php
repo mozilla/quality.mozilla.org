a9184b6928ae90c9cca627f8e5d9772b03b8b08a
craigcook@gmail.com <craigcook@gmail.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-12-01 10:26:39 +0000

    Tabbed sub-teams
    git-svn-id: http://svn.mozilla.org/projects/quality.mozilla.org/branches/wordpress@78509 4eb1ac78-321c-0410-a911-ec516a8615a5

--- wp-content/themes/QMO3/groups/single/front.php
+++ wp-content/themes/QMO3/groups/single/front.php
@@ -1,32 +1,50 @@
 <?php 
-  global $bp, $forum_template;
   $page_slug = bp_get_group_slug();
   $page_id = get_page_by_path($page_slug)->ID;
   fc_get_post($page_id);
 ?>
 <?php if ($page_id != '') : ?>
-<article class="team-general single">
-  <h1 class="entry-title"><?php the_title(); ?></h1>
-  <div class="entry-content">
-  <?php the_content(); ?>
+
+  <div class="tabbed team-general">
+    <h1 class="team-title"><?php the_title(); ?></h1>
+    <div class="entry-content">
+    <?php the_content(); ?>
+    </div>
+
+  <?php $children = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_parent = ".$page_id." AND post_type = 'page' ORDER BY menu_order", 'OBJECT'); ?>
+  <?php if ( $children ) : ?>
+    <?php foreach ( $children as $child ) : setup_postdata( $child ); ?>
+      <h1 class="team-title"><?php echo $child->post_title; ?></h1>
+      <div class="entry-content">
+      <?php the_content(); ?>
+      </div>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
   </div>
 
-  <?php $children = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_parent = ".$post->ID."   AND post_type = 'page' ORDER BY menu_order", 'OBJECT'); ?>
   <?php if ( $children ) : ?>
-  <ul id="sub-teams" class="groups hfeed">
-    <?php foreach ( $children as $child ) : setup_postdata( $child ); ?>
-    <li class="team hentry">
-      <h2 class="entry-title"><a href="<?php echo get_permalink($child->ID); ?>" rel="bookmark" title="<?php echo $child->post_title; ?>"><?php echo $child->post_title; ?></a></h2>
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div>
-    </li>
-    <?php endforeach; ?>
-  </ul>
+    <script src="<?php bloginfo('stylesheet_directory'); ?>/TabInterface.js" type="text/javascript"></script>
+    <script type="text/javascript">
+    // <![CDATA[
+      jQuery(document).ready(function(){
+        var cabinets = Array();
+        var collection = document.getElementsByTagName( '*' );
+        var cLen = collection.length;
+        for( var i=0; i<cLen; i++ ){
+          if( collection[i] &&
+              /\s*tabbed\s*/.test( collection[i].className ) ){
+            cabinets.push( new TabInterface( collection[i], i ) );
+          }
+        }
+      });
+    // ]]>
+    </script>
   <?php endif; ?>
-      
-</article>
+
 <?php else : ?>
+
   <h2><?php bp_group_name(); ?></h2>
   <p><?php bp_group_member_count(); ?> <?php bp_group_join_button(); ?></p>
+
 <?php endif; ?>
