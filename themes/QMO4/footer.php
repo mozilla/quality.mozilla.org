c2fcfa4342e4528c6d580dc7cef6ada4fd1f2de0
davd Weir <Satdav.mozilla@gmail.com>
2013-02-02 01:01:01 +0000

    Update themes/QMO4/footer.php
    fixed the bug

--- themes/QMO4/footer.php
+++ themes/QMO4/footer.php
@@ -8,8 +8,7 @@
     <p>Portions of QMO content are &copy; 1998&ndash;<?php echo date('Y');?> by individual mozilla.org contributors.</p>
     <p>Some content available under a <a href="http://www.mozilla.org/foundation/licensing/website-content.html" rel="external license">Creative Commons license</a></p>
   <?php if (get_page_by_path('about')) : ?>
-   If you would like to help with coding of QA  please see the following a href="https://github.com/mozilla/quality.mozilla.org" rel="Conttribute to the code">Contribute to the code</a></p>
-  <?php if (get_page_by_path('about')) : ?>
+   If you would like to help with coding of QA  please see the following a href="https://github.com/mozilla/quality.mozilla.org" rel="Contribute to the code">Contribute to the code</a></p>
 
     <p><a href="<?php echo get_permalink(get_page_by_path('about')->ID); ?>"><?php echo get_page_by_path('about')->post_title; ?></a></p>
   <?php endif; ?>
